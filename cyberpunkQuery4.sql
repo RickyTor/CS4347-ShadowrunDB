353c22596f13a306aa8a76df401f8457cda83eae
gmw93 <49510885+gmw93@users.noreply.github.com>
2019-05-02 22:13:25 -0500

    Update cyberpunkQuery4.sql

--- cyberpunkQuery4.sql
+++ cyberpunkQuery4.sql
@@ -1,7 +1,12 @@
-SELECT Username, Name, TotalKarma, KarmaSpent, TotalFunds, FundsSpent
+-- prints off user and character data, but not character description
+SELECT Username, Name, TotalKarma, KarmaSpent, TotalFunds, FundsSpent, Willpower, Logic, Intuition, Charisma, Edge, EdgeAvailable, Essence, PriorityA, PriorityB, PriorityC, PriorityD, PriorityE
 FROM USERS, CHARACTERS
 WHERE USERS.Username = CHARACTERS.Username_FK;
+-- old queries, same as above but in two seperate queries
+-- SELECT Username, Name, TotalKarma, KarmaSpent, TotalFunds, FundsSpent
+-- FROM USERS, CHARACTERS
+-- WHERE USERS.Username = CHARACTERS.Username_FK;
 
-SELECT Username, Name, Willpower, Logic, Intuition, Charisma, Edge, EdgeAvailable, Essence, PriorityA, PriorityB, PriorityC, PriorityD, PriorityE
-FROM USERS, CHARACTERS
-WHERE USERS.Username = CHARACTERS.Username_FK;
+-- SELECT Username, Name, Willpower, Logic, Intuition, Charisma, Edge, EdgeAvailable, Essence, PriorityA, PriorityB, PriorityC, PriorityD, PriorityE
+-- FROM USERS, CHARACTERS
+-- WHERE USERS.Username = CHARACTERS.Username_FK;
